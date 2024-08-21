ba8bbcb8e5e1dc9acd9887ddf949aacdec934ef8
Jakka Balaji Mahendra <85291879+NameRectified@users.noreply.github.com>
2024-08-21 15:05:40 +0530

    Initial commit

---
+++ biLinearInterpolation.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import math
+
+image = cv2.imread('demo-LRImage.png')
+
+height,width = image.shape[0:2]
+if (len(image.shape)==3):
+    channels = 3
+else:
+    channels =1
+
+scaling_factor =3
+new_height = height*scaling_factor
+new_width = width*scaling_factor
+
+# empty array
+outputImage = np.zeros((new_height,new_width,channels),dtype=np.uint8)
+
+# mapping
+for y in range(new_height):
+    for x in range(new_width):
+        x_orig = x/scaling_factor
+        y_orig = y/scaling_factor
+        # four nearest pixels
+        x_one = math.floor(x_orig)
+        x_two = math.ceil(x_orig)
+        x_two = min(x_two,width-1)  #to handle edge cases where the value may go beyond the boundaries
+        y_one = math.floor(y_orig)
+        y_two = math.ceil(y_orig)
+        y_two = min(y_two,height-1)  #to handle edge cases where the value may go beyond the boundaries
+        # fractional distances
+        d_x = x_orig - x_one
+        d_y = y_orig - y_one
+
+        # pixel values from original image
+        # 11 means y=1,x=1 and so on
+        I_11 = image[y_one,x_one]
+        I_12 = image[y_one,x_two]
+        I_21 = image[y_two,x_one]
+        I_22 = image[y_two,x_two]
+
+        # interpolating along x axis
+        I_top = I_11*(1-d_x) + I_12 * d_x
+        I_bottom = I_21*(1-d_x) + I_22*d_x
+
+        # interpolating along y axis
+        I_final = I_top * (1-d_y) + I_bottom*d_y
+        outputImage[y,x] = I_final
+
+cv2.imshow('Original Image',image)
+cv2.imshow('Resized Image',outputImage)
+cv2.waitKey(0)
---
+++ nearestNeighborInterpolation.py
@@ -0,0 +1,67 @@
+import cv2 #for reading image
+import numpy as np
+import math
+import matplotlib.pyplot as plt # for comparing input and output side by side
+image = cv2.imread('demo-LRImage.png')
+
+# to view the imported image
+# cv2.imshow('image',lrImage)
+# to show the output until you press a key (if you dont put this the output is shown very fast and close)
+# cv2.waitKey(0)
+
+# .shape consists of the number of rows(height), columns(width), number of channels present in the image
+# print(lrImage.shape)
+# if len==2 it means it is grayscale, if it is 3 it means it is RGB
+# print(len(lrImage.shape))
+
+# height = lrImage.shape[0]
+# width = lrImage.shape[1]
+height,width = image.shape[:2]
+if(len(image.shape)==3):
+    channels = image.shape[2]
+else:
+    channels = 1
+
+# scaling factor
+scaling_factor =4
+# new dimensions
+new_height = height*scaling_factor
+new_width = height*scaling_factor
+
+# empty output image array
+outputImg = np.zeros((new_height,new_width,channels),dtype=np.uint8)
+# print(outputImg)
+
+for y in range(new_height):
+    for x in range(new_width):
+        # print("Y:",y,"X",x)
+        mapped_x = math.floor(x/scaling_factor)
+        mapped_y = math.floor(y/scaling_factor)
+        # print(mapped_x,mapped_y)
+        orgPixelValue = image[mapped_y,mapped_x]
+        outputImg[y,x] = orgPixelValue
+
+cv2.imshow('Original Image',image)
+cv2.imshow('Resized Image',outputImg)
+# cv2.waitKey(0)
+
+# matplotlib expects rgb scheme, where as in cv2 it is bgr scheme
+input_image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+output_image_rgb = cv2.cvtColor(outputImg, cv2.COLOR_BGR2RGB)
+
+# Plot images
+plt.figure(figsize=(10, 5))  # Adjust figure size as needed
+
+# Input Image
+plt.subplot(1, 2, 1)
+plt.title('Original Image')
+plt.imshow(input_image_rgb)
+plt.axis('on')
+
+# Resized Image
+plt.subplot(1, 2, 2)
+plt.title('Resized Image')
+plt.imshow(output_image_rgb)
+plt.axis('on')
+
+plt.show()
